5a93528360c955ad68060af6421766d98da786ac
Dave <davidalejandrodand@gmail.com>
2022-07-23 11:08:56 -0400

    add prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -26,3 +26,46 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+#============= lOGGING SYSTEM ================#
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False, 
+    'formatters':{
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'Simple_Format':{
+            'format': '{levelname} {message}',
+            'style': '{',
+        }
+    },
+ 
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+            # 'class': 'logging.FileHandler',
+            # 'filename': './logs/log_file1.log',
+            'formatter':'Simple_Format',
+        },
+ 
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+ 
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+
+}
