ffcae39a5598a2cc3fb44bdb9b1c3e85173b9cc8
Dave <davidalejandrodand@gmail.com>
2022-07-25 08:02:51 -0400

    changing the path on command set_img_clients

--- apps/clients/management/commands/set_img_clients.py
+++ apps/clients/management/commands/set_img_clients.py
@@ -22,8 +22,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for cli in Cliente.objects.all():
             c = random.choice([1, 2, 3, 4, 5])
-            avatar_file_content = get_file_obj(f"src/img/avatar{c}.png", "PNG") 
-            dni_photo_file_content = get_file_obj(f"src/img/480.jpg", "JPEG") 
+            avatar_file_content = get_file_obj(f"static/src/img/avatar{c}.png", "PNG") 
+            dni_photo_file_content = get_file_obj(f"static/src/img/480.jpg", "JPEG") 
 
             cli.avatar.save(f"avatar{c}.png", avatar_file_content) 
             cli.foto_dni.save("480.jpg", dni_photo_file_content) 
